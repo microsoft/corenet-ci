1b2e4ea05bb2fb1cd68f73a779e65cf30be1ecc7
Nick Banks <nibanks@microsoft.com>
2021-09-13 14:08:04 -0400

    More tab to space

--- vm-setup/CoreNet-CI-Startup.ps1
+++ vm-setup/CoreNet-CI-Startup.ps1
@@ -15,36 +15,36 @@ $RemoteDumpDir = "\\anrossi-azp\bugchecks"
 $RemoteSharePassword = "VM-Test-Execution"
 
 try {
-	# Copy out any dump file.
-	if (Test-Path $LocalDumpPath) {
-		$RemoteDumpPath = ""
-		if (Test-Path $LocalDumpCfgPath) {
-			# Use the dump config to copy the dump out.
-			$Description = Get-Content $LocalDumpCfgPath
-			$RemoteDumpPath = Join-Path $RemoteDumpDir $env:computername ($Description + ".dmp")
-			
-		} else {
-			# No explicit dump configuration. Generate a generic one.
-			$DateTime = Get-Date -Format "yyyy.dd.MM.HH.mm.ss"
-			$RemoteDumpPath = Join-Path $RemoteDumpDir $env:computername ("memory" + $DateTime + ".dmp")
-		}
-		
-		Write-Debug "Copying $LocalDumpPath to $RemoteDumpPath"
-		
-		# Make sure the share is available.
-		net use $RemoteDumpDir $RemoteSharePassword /USER:VM
-		
-		# Copy the file out.
-		Copy-Item $LocalDumpPath $RemoteDumpPath -Recurse -Force
-		
-		# Delete the local file.
-		Remove-Item $LocalDumpPath -Force
-	}
+    # Copy out any dump file.
+    if (Test-Path $LocalDumpPath) {
+        $RemoteDumpPath = ""
+        if (Test-Path $LocalDumpCfgPath) {
+            # Use the dump config to copy the dump out.
+            $Description = Get-Content $LocalDumpCfgPath
+            $RemoteDumpPath = Join-Path $RemoteDumpDir $env:computername ($Description + ".dmp")
+            
+        } else {
+            # No explicit dump configuration. Generate a generic one.
+            $DateTime = Get-Date -Format "yyyy.dd.MM.HH.mm.ss"
+            $RemoteDumpPath = Join-Path $RemoteDumpDir $env:computername ("memory" + $DateTime + ".dmp")
+        }
+        
+        Write-Debug "Copying $LocalDumpPath to $RemoteDumpPath"
+        
+        # Make sure the share is available.
+        net use $RemoteDumpDir $RemoteSharePassword /USER:VM
+        
+        # Copy the file out.
+        Copy-Item $LocalDumpPath $RemoteDumpPath -Recurse -Force
+        
+        # Delete the local file.
+        Remove-Item $LocalDumpPath -Force
+    }
 
 } catch {
-	Write-Debug "Encountered exception!"
+    Write-Debug "Encountered exception!"
 
 } finally {
-	# Start AZP agent service
-	Start-Service (Get-Service vstsagent*)
+    # Start AZP agent service
+    Start-Service (Get-Service vstsagent*)
 }
