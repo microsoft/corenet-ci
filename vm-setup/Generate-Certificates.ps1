000cb279fc1ac0a3f3de46fa97abb27c26505f82
Nick Banks <nibanks@microsoft.com>
2022-09-27 11:34:43 -0400

    Update signing certificates

--- vm-setup/Generate-Certificates.ps1
+++ vm-setup/Generate-Certificates.ps1
@@ -21,7 +21,7 @@ $CodeSignCert = New-SelfSignedCertificate `
     -KeyUsage DigitalSignature `
     -KeyUsageProperty Sign `
     -TextExtension @("2.5.29.37={text}1.3.6.1.5.5.7.3.3,1.3.6.1.4.1.311.10.3.6","2.5.29.19 = {text}") `
-    -NotAfter (Get-Date).AddYears(1) `
+    -NotAfter (Get-Date).AddYears(10) `
     -KeyExportPolicy Exportable `
     -Type Custom
 
